9b7b3cba84ca326e765c6d3b3656f7541a0cfdca
Veronica Kodehode <Veronicas.kodehode@gmail.com>
2024-03-20 10:09:20 +0100

    simple color picker

---
+++ script.js
@@ -0,0 +1,39 @@
+const body = document.getElementById("body");
+const random = document.getElementById("random");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const red = document.getElementById("red");
+
+blue.addEventListener("click", function () {
+  body.style.backgroundColor = "rgb(67, 67, 224)";
+});
+
+yellow.addEventListener("click", function () {
+  body.style.backgroundColor = "yellow";
+});
+
+red.addEventListener("click", function () {
+  body.style.backgroundColor = "red";
+});
+
+function changeToGreen() {
+  body.style.backgroundColor = "green";
+}
+green.addEventListener("click", function () {
+  body.style.backgroundColor = "green";
+});
+
+function changeToRandom() {
+  const randomColor =
+    "rgb(" +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    ")";
+
+  body.style.backgroundColor = randomColor;
+}
+random.addEventListener("click", changeToRandom);
